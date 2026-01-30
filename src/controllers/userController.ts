674443037c99afea9294a23771abcdc48bf3de75
mohamednagy54 <mnagy14966@gmail.com>
2026-01-30 21:46:48 +0200

    fix: fixed auth problem

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -53,7 +53,7 @@ export const registerUser = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -106,7 +106,7 @@ export const loginUser = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -146,7 +146,7 @@ export const logoutUser = async (
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     res
